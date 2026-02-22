83c187a21265eff917adffed28c3b928a918c4a3
Leo Li <lyiheng3@gmail.com>
2026-02-21 20:55:35 -0800

    Initial Commit

---
+++ Tiny_reminder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tiny_reminder
+//
+//  Created by Leo Li on 2/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tiny_reminder/Tiny_reminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tiny_reminderApp.swift
+//  Tiny_reminder
+//
+//  Created by Leo Li on 2/21/26.
+//
+
+import SwiftUI
+
+@main
+struct Tiny_reminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
